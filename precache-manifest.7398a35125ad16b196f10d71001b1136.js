1f55193d4fa51c8beae9bc67d01965b6208d2927
sammer119 <61250062+sammer119@users.noreply.github.com>
2020-08-18 16:44:23 +0100

    Add files via upload

---
+++ precache-manifest.7398a35125ad16b196f10d71001b1136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd370795d6056a29d0d8d679e0fee34f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c2287f3ecc41a8bc4ba9",
+    "url": "./static/css/main.09f3c54e.chunk.css"
+  },
+  {
+    "revision": "c6030c45a8bbad5f0db7",
+    "url": "./static/js/2.b6a5d628.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b6a5d628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2287f3ecc41a8bc4ba9",
+    "url": "./static/js/main.404ae7bd.chunk.js"
+  },
+  {
+    "revision": "eaaaeb3baf1bd063a281",
+    "url": "./static/js/runtime-main.28e5a54c.js"
+  }
+]);
